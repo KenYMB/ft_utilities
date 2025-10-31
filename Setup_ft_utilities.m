b895a253ac1b7b43aaccdf07896f8316880f97b8
KenYMB <yuasa.research@gmail.com>
2025-10-31 16:24:41 +0900

    update setup function

--- Setup_ft_utilities.m
+++ Setup_ft_utilities.m
@@ -5,71 +5,38 @@ function    varargout = Setup_ft_utilities
 %   oldpath = SETUP_FT_UTILITIES;
 
 % 20180517 Yuasa
+% 20251031 Yuasa: major update on the recent environment
 
 nargoutchk(0,1);
 
-rootdir = fileparts(which(mfilename));
-
 %-- add ft_utilities
-adddir = {'ft_utilities', 'fieldtrip_utilities'};
-locpos = {'.','..'};
-  for idir = 1:length(adddir)   % check in locpos
-    addexist = false(1,length(locpos));
-    for iloc = 1:length(locpos)
-        addexist(idir) = exist(fullfile(rootdir,locpos{iloc},adddir{idir}),'dir');
-    end
-    isadd = find(addexist,1);
-    if ~isempty(isadd)
-        pathlist = genpath(absolute_path(fullfile(rootdir,locpos{iloc},adddir{idir})));
-        break;
-    end
-  end
-    
-assert(~isempty(isadd), 'failed to find fieldtrip_utilities directory');
+rootdir = fileparts(which(mfilename));
+pathlist = genpath(rootdir);
 pathlist = strsplit(pathlist,pathsep)';
+filesepexp =  regexptranslate('escape',filesep);
 
-%-- remove calibration related dir
-for idir = length(pathlist):-1:1
-    if ~isempty(strfind(pathlist{idir},'CalibratedData')) || ~isempty(strfind(pathlist{idir},'mytransformation'))
-        pathlist(idir) = [];
-    end
-end
+%-- remove hidden dir
+idx = ~cellfun(@isempty,regexp(pathlist,[filesepexp '\..'],'once'));
+pathlist(idx) = [];
 
 %-- remove private dir
-for idir = length(pathlist):-1:1
-    if strfind(pathlist{idir},'ft_private')
-        pathlist(idir) = [];
-    end
-end
+idir = ~cellfun(@isempty,regexp(pathlist,[filesepexp 'ft_private'],'once'));
+pathlist(idir) = [];
 
 %-- set low priority for ft_fix
-lowPpath = {};
-for idir = length(pathlist):-1:1
-    if strfind(pathlist{idir},'ft_fix')
-        lowPpath       = [lowPpath; pathlist(idir)];
-        pathlist(idir) = [];
-    end
-end
+idir = ~cellfun(@isempty,regexp(pathlist,[filesepexp 'ft_fix'],'once'));
+lowPpath = pathlist(idir);
+pathlist(idir) = [];
 
 %-- output
 pathlist  = strjoin(pathlist,pathsep);
 lowPpath  = strjoin(lowPpath,pathsep);
 oldpath   = addpath(pathlist);
 addpath(lowPpath,'-END');
-savepath;
+% savepath;
 
 if nargout>0
     varargout{1} = oldpath;
 end
-
-end
-
-function APATH = absolute_path(RPATH) 
-
-if ~ischar(RPATH), error('Input value is not path.'); end; 
-
-[dirname, filename, ext] = fileparts(RPATH);
-[num,pathinfo] = fileattrib(dirname);
-APATH = fullfile(pathinfo.Name, strcat([filename, ext]));
 
 end
